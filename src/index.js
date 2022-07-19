1a2317c721a4fe00f7f23b5713ff430421548ee3
Katsuraji1 <verahovets@gmail.com>
2022-07-19 22:45:42 +0300

    docs: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ if (str.length%2){
+  return false;
+ }
+ let stack_array=[];
+ for (let i=0;i<str.length;i++){
+  for (let j=0;j<bracketsConfig.length;j++){
+    if (str[i]===bracketsConfig[j][1] && stack_array[stack_array.length-1]===bracketsConfig[j][0]){
+      stack_array.pop();
+    }
+    else {
+      if(str[i]===bracketsConfig[j][0]){
+stack_array.push(str[i]);
+      }
+    }
+  }
+ }
+ return stack_array.length ? false:true;
 }
